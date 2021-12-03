085b019dae5e94353918a123c6a2f42d6ec1632a
Qalb E Habib <rana.habib313@gmail.com>
2021-12-03 21:46:54 +0500

    First COmmit

---
+++ script.js
@@ -0,0 +1,106 @@
+// document.write("<h1>hellow world, this is qalbehabib</h1>")
+
+// console.log('hellow world')
+
+// let a = "habib"
+
+// console.log(a + " welcone to the javascript course")
+
+
+// a = 12
+
+// console.log(a)
+
+
+// a += 43;
+// console.log(a)
+
+// let listofitems  =  ['Apple', 'banana' , 'mango' , 'orenge']
+
+// console.log(listofitems[3])
+
+// let person = {
+//     name:'rana qalbehabib',
+//     age: 23,
+//     height:5.6,
+//     weigth:55
+// }
+
+// console.log(`my name is ${person.name} and age is ${person.age} and with the height of ${person.height} inches and
+// weigth of ${person.weigth}kg`)
+
+
+
+// console.log(document.getElementById('habib').innerHTML = "my name is qalbehbib")
+// let name = prompt("enter your Name")
+// function habib(c ,d ){
+//     var result = "the functoin is now a variable"
+//     d = c + d
+
+//     console.log(name)
+
+//     console.log(d)
+// }
+
+// habib('HEllow ','this is qalbehabib')
+
+// let num = 0;
+
+// while(num < 100)
+// {
+//     alert
+//     num++;
+//     console.log(num)
+// console.log("while loop is running")
+// }
+
+
+// num = 1;
+
+// while(num < 100)
+// {
+//     alert
+//     num+=1;
+//     console.log(num)
+// console.log("while loop is running")
+// }
+
+
+
+//  
+
+
+// for(i = 0 ; i<=100 ; i++){
+//     console.log("for loop running " + i  + " time" )
+// }
+
+
+//Data Type 
+
+let number= 23; // Number Data Type
+let name = 'Qalbe HAbib is my name'; // String Data type 
+let flot= 45.34; //  Float  data type
+let truth = false; // Boolean Data type
+let grocery = ['appele' , 'bananaa' , 'orange' , 'mangos'] // Array  data type
+let obj = {
+    name : 'Qaleb habib',
+    age:'23',
+    status:"single",
+    city: "faisalabad"
+} // Object data type
+
+let random; // Undefined data type
+let nothing ; // null
+
+
+
+
+
+
+
+
+//String
+
+let newstr= "Banana";
+
+console.log(newstr.indexOf(''))
